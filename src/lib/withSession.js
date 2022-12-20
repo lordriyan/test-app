62dda9b8419668053776d514858e8a3ee05dc000
Riyan Saputra <alamat.emailnya.riyan@gmail.com>
2022-12-20 04:06:26 +0000

    fix cookie options

--- src/lib/withSession.js
+++ src/lib/withSession.js
@@ -1,20 +1,18 @@
 import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
 
 const sessionOptions = {
-  password: "complex_password_at_least_32_characters_long",
-  cookieName: "myapp_cookiename",
+  password: "DWEXGZH2J4M5N6Q8R9SBUCVDWFYGZHZ3",
+  cookieName: "KUEKU",
   // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
   cookieOptions: {
-    secure: false //process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "production",
   },
 };
 
 export function withSessionRoute(handler) {
-  console.log(process.env.NODE_ENV);
   return withIronSessionApiRoute(handler, sessionOptions);
 }
 
 export function withSessionSsr(handler) {
-  console.log(process.env.NODE_ENV);
   return withIronSessionSsr(handler, sessionOptions);
 }
